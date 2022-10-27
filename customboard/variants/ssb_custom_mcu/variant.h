cb288d96a8db4ecd7dbf11ad0544b99085648cd3
John Taylor <john.taylor@epfl.ch>
2022-10-27 14:41:04 +0200

    added right variants

--- customboard/variants/ssb_custom_mcu/variant.h
+++ customboard/variants/ssb_custom_mcu/variant.h
@@ -57,10 +57,10 @@ extern "C"
  *----------------------------------------------------------------------------*/
 
 // Number of pins defined in PinDescription array
-#define PINS_COUNT           (44u)
+#define PINS_COUNT           (90u)
 #define NUM_DIGITAL_PINS     (23u)
-#define NUM_ANALOG_INPUTS    (6u)
-#define NUM_ANALOG_OUTPUTS   (1u)
+#define NUM_ANALOG_INPUTS    (10u)
+#define NUM_ANALOG_OUTPUTS   (2u)
 #define analogInputToDigitalPin(p)  ((p < 6u) ? (p) + 14u : -1)
 
 #define digitalPinToPort(P)        ( &(PORT->Group[g_APinDescription[P].ulPort]) )
@@ -80,47 +80,95 @@ extern "C"
  */
 // #define digitalPinToTimer(P)
 
-// LEDs
-#define PIN_LED_13           (13u)
-#define PIN_LED_RXL          (25u)
-#define PIN_LED_TXL          (26u)
-#define PIN_LED              PIN_LED_13
-#define PIN_LED2             PIN_LED_RXL
-#define PIN_LED3             PIN_LED_TXL
-#define LED_BUILTIN          PIN_LED_13
-#define PIN_NEOPIXEL         (8)
-#define PIN_NEOPIXEL_POWER   (7)
+// Port define : Pin header for shield
+#define PA02                 (50u) //A0
+#define PA03                 (51u) // A5 // AREF
+#define PB04                 (44u) //SWITCH_BUILTIN
+#define PB05                 (45u) //LED_BUILTIN
+#define PB06                 (46u) //A7
+#define PB07                 (52u) //A8
+#define PB08                 (53u) //A2
+#define PB09                 (54u) //A3
+#define PA04                 (55u) //A4
+#define PA05                 (56u) //A1
+#define PA12                 (57u) //SDA1
+#define PA13                 (58u) //SCL1
+#define PB03                 (59u)
+#define PB02                 (60u)
+#define PB01                 (61u) //A9
+#define PB00                 (62u)
+#define PB31                 (63u)
+#define PB30                 (64u)
+#define PA27                 (65u)
+#define PB23                 (66u) //MOSI
+#define PB22                 (67u) //MISO
+#define PA19                 (68u)
+#define PA18                 (69u)
+#define PA17                 (70u) //SCL0 //SCK
+#define PA16                 (71u) //SDA0
+
+#define PB17                 (72u) //LED_RX_CAN0
+#define PB16                 (73u) //LED_TX_CAN0
+#define PB13                 (74u) //LED_RX_CAN1
+#define PB12                 (75u) //LED_TX_CAN1
+
+#define PA14                 (76u) //FAULT_CAN1
+#define PA15                 (77u) //S_CAN1
+#define PA20                 (78u) //FAULT_CAN0
+#define PA21                 (79u) //S_CAN0
+
+#define PA06                 (80u) //SD_CP
+#define PA07                 (81u) //SD_WP
+#define PA08                 (82u) //SD_CMD
+#define PA09                 (83u) //SDDAT0
+#define PA10                 (84u) //SD_DAT1
+#define PA11                 (85u) //SD_DAT2
+#define PB10                 (86u) //SD_DAT3
+#define PB11                 (87u) //SD_CLK
+
+// LED AND SWITCH
+#define LED_BUILTIN          PB05
+#define SWITCH_BUILTIN       PB04
+#define LED_RX_CAN0          PB17
+#define LED_TX_CAN0          PB16
+#define LED_RX_CAN1          PB13
+#define LED_TX_CAN1          PB12
 
 /*
  * Analog pins
  */
-#define PIN_A0               (14ul)
-#define PIN_A1               (PIN_A0 + 1)
-#define PIN_A2               (PIN_A0 + 2)
-#define PIN_A3               (PIN_A0 + 3)
-#define PIN_A4               (PIN_A0 + 4)
-#define PIN_A5               (PIN_A0 + 5)
-#define PIN_A6               (PIN_A0 + 6)
+ #define PIN_A0               (14ul) // alternative use of PA02
+ #define PIN_A1               (PIN_A0 + 1) // alternative use of PA05
+ #define PIN_A2               (PIN_A0 + 2) // alternative use of PAB08
+ #define PIN_A3               (PIN_A0 + 3) // alternative use of PB09
+ #define PIN_A4               (PIN_A0 + 4) // alternative use of PA04
+ #define PIN_A5               (PIN_A0 + 5) // alternative use of PA03
+ #define PIN_A6               (PIN_A0 + 6) // alternative use of PB00
+ #define PIN_A7               (47u) // alternative use of PB06
+ #define PIN_A8               (48u) // alternative use of PB07
+ #define PIN_A9               (49u) // alternative use of
+ static const uint8_t A0  = PIN_A0;
+ static const uint8_t A1  = PIN_A1;
+ static const uint8_t A2  = PIN_A2;
+ static const uint8_t A3  = PIN_A3;
+ static const uint8_t A4  = PIN_A4;
+ static const uint8_t A5  = PIN_A5;
+ static const uint8_t A6  = PIN_A6 ;
+ static const uint8_t A7  = PIN_A7;
+ static const uint8_t A8  = PIN_A8;
+ static const uint8_t A9  = PIN_A9 ;
 
-#define PIN_DAC0             (14ul)
-#define PIN_DAC1             PIN_A1
-
-static const uint8_t A0  = PIN_A0;
-static const uint8_t A1  = PIN_A1;
-static const uint8_t A2  = PIN_A2;
-static const uint8_t A3  = PIN_A3;
-static const uint8_t A4  = PIN_A4;
-static const uint8_t A5  = PIN_A5;
-static const uint8_t A6  = PIN_A6 ;
-
+#define PIN_DAC0             (32u) //DAC0 alternative use of PA02
+#define PIN_DAC1             (33u) //DAC1 alternative use of PA05
 static const uint8_t DAC0 = PIN_DAC0;
 static const uint8_t DAC1 = PIN_DAC1;
 
 #define ADC_RESOLUTION		12
 
-// Other pins
-#define PIN_ATN              (31ul)
-static const uint8_t ATN = PIN_ATN;
+// AREF
+#define PIN_ATN              (31ul) //aletrnative use of PA03
+static const uint8_t ATN = PIN_ATN; //PA03
+static const uint8_t AREF = PIN_ATN; // PA03
 
 /*
  * Serial interfaces
@@ -163,8 +211,14 @@ static const uint8_t SCK  = PIN_SPI_SCK ;
 #define WIRE_IT_HANDLER_2    SERCOM2_2_Handler
 #define WIRE_IT_HANDLER_3    SERCOM2_3_Handler
 
-static const uint8_t SDA = PIN_WIRE_SDA;
-static const uint8_t SCL = PIN_WIRE_SCL;
+static const uint8_t SDA1 = PIN_WIRE_SDA; //PA12
+static const uint8_t SCL1 = PIN_WIRE_SCL; //PA13
+
+
+// A FINALISER POUR AVOIR 2 I2C
+static const uint8_t SDA0 = PA16; //PA16
+static const uint8_t SCL0 = PA17; //PA17
+
 
 /*
  * USB
@@ -173,25 +227,26 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 #define PIN_USB_DM          (29ul)
 #define PIN_USB_DP          (30ul)
 
+
 /*
  * I2S Interfaces
  */
-#define I2S_INTERFACES_COUNT 1
+//#define I2S_INTERFACES_COUNT 1
 
-#define I2S_DEVICE          0
-#define I2S_CLOCK_GENERATOR 3
+//#define I2S_DEVICE          0
+//#define I2S_CLOCK_GENERATOR 3
 
-#define PIN_I2S_SDO          (11u)
-#define PIN_I2S_SDI          (12u)
-#define PIN_I2S_SCK          PIN_SERIAL1_TX
-#define PIN_I2S_FS           (10u)
-#define PIN_I2S_MCK          PIN_SERIAL1_RX
+//#define PIN_I2S_SDO          (11u)
+//#define PIN_I2S_SDI          (12u)
+//#define PIN_I2S_SCK          PIN_SERIAL1_TX
+//#define PIN_I2S_FS           (10u)
+//#define PIN_I2S_MCK          PIN_SERIAL1_RX
 
-// On-board QSPI Flash
+// On-board QSPI Flash -> à modifier pour que ça marche avec une carte SD !
 #define EXTERNAL_FLASH_DEVICES   GD25Q16C
 #define EXTERNAL_FLASH_USE_QSPI
 
-//QSPI Pins
+//QSPI Pins -> déjà été modifié dans .cpp
 #define PIN_QSPI_SCK    (34u)
 #define PIN_QSPI_CS     (35u)
 #define PIN_QSPI_IO0    (36u)
@@ -207,13 +262,15 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 /*
  * CAN
  */
-#define PIN_CAN_STANDBY (40)
-#define PIN_CAN_BOOSTEN (41)
+ //CAN1
+#define PIN_CAN_STANDBY (40) // voir si le code fonctionne sans
+#define PIN_CAN_BOOSTEN (41) // voir si le code fonctionne sans
 #define PIN_CAN_TX      (42)
 #define PIN_CAN_RX      (43)
 
-#define PIN_CAN1_TX     (12)
-#define PIN_CAN1_RX     (13)
+//CAN0
+#define PIN_CAN1_TX     (88) //(12)
+#define PIN_CAN1_RX     (89) //(13)
 
 
 #ifdef __cplusplus
@@ -263,4 +320,3 @@ extern Uart Serial1;
 #define SERIAL_PORT_HARDWARE_OPEN   Serial1
 
 #endif /* _VARIANT_FEATHER_M4_ */
-
