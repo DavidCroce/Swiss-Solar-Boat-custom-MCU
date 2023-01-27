271ecdfcef06f431f6f3557ee6d0efce8cf9e18a
JohnTomasTaylor <115045383+JohnTomasTaylor@users.noreply.github.com>
2023-01-27 09:31:07 +0100

    Add files via upload
    changed variants

--- customboard/variants/ssb_custom_mcu/variant.h
+++ customboard/variants/ssb_custom_mcu/variant.h
@@ -52,15 +52,131 @@ extern "C"
 {
 #endif // __cplusplus
 
+
+enum Pinsymbol {
+
+  // SERCOM/UART (Serial1) / Also used by CAN_LED RX/TX
+
+  PIN_SERIAL1_RX = 0,      // PB17
+  PIN_SERIAL1_TX,          // PB16
+
+  // Analog pins 
+  // ----------- ADC0 ------------
+
+  PIN0_ADC0, // PA02
+  PIN1_ADC0, // PA03
+  PIN2_ADC0, // PA05
+  PIN3_ADC0, // PA04 (has PWM)
+  PIN4_ADC0, // PB00
+  PIN5_ADC0, // PB01 (has PWM)
+  PIN6_ADC0, // PB02 (has PWM)
+  PIN7_ADC0, // PB03 (has PWM)
+  
+  //  ----------- ADC1 ------------
+ 
+  PIN0_ADC1, // PB04 (SWITCH on SHIELD)
+  PIN1_ADC1, // PB05 (LED_G on SHIELD)
+  PIN2_ADC1, // PB06
+  PIN3_ADC1, // PB07
+  PIN4_ADC1, // PB08 (has PWM)
+  PIN5_ADC1, // PB09 (has PWM)
+
+  //  ----------- I2C pins (SDA,SCL) ----------------
+
+  SDA0, // PA16
+  SCL0, // PA17
+  SDA1, // PA12
+  SCL1, // PA13
+
+  // SPI pins (MISO,MOSI,SCK)
+  // ----------------------
+
+  MISO, // PB22
+  MOSI, // PB23
+  SCK,  // PA17 (also SCL0 should be changed)
+
+  // --------- USB -----------
+
+  USB_HOST_ENABLE,  // Not used
+  USB_DM,           
+  USB_DP,
+
+  // 31 (AREF)
+
+  AREF,             // PA03
+
+  // --------- DAC -----------
+
+  DAC0,             // PA02
+  DAC1,             // PA05
+
+  // ------- QSPI (SCK, CS, IO0, IO1, IO2, IO3) ---------
+
+  ENUM_QSPI_SCK,
+  ENUM_CS,
+  IO0,
+  IO1,
+  IO2,
+  IO3,
+
+  // --------- CAN0 (TX, RX) ----------
+
+  PIN_CAN0_TX,  // PA22 (CAN0_TX on SHIELD)
+  PIN_CAN0_RX,  // PA23 (CAN0_RX on SHIELD)
+
+  // --------- CAN1 (TX, RX) ----------
+
+  PIN_CAN1_TX,  // PB14 (CAN1_TX on SHIELD)
+  PIN_CAN1_RX,  // PA15 (CAN1_RX on SHIELD)
+
+  // --------- Digital pins -----------
+
+  // PORTA
+
+  PA02, // also DAC0
+  PA03, // also AREF
+  PA04, // (has PWM)
+  PA05, // also DAC1 (has PWM)
+  PA12, // also SDA1 (has PWM)
+  PA13, // also SCL1 (has PWM)
+  PA16, // also SDA0 (has PWM)
+  PA17, // also SCL0 (has PWM)
+  PA18, // (has PWM)
+  PA19, // (has PWM)
+  PA22, // also CAN0_TX (has PWM)
+  PA23, // also CAN0_RX (has PWM)
+  PA27,
+
+  PB00,
+  PB01,
+  PB02,
+  PB03,
+  PB04,
+  PB05,
+  PB06,
+  PB07,
+  PB08,
+  PB09,
+  PB14,
+  PB15,
+  PB22,
+  PB23,
+  PB30,
+  PB31,
+
+
+
+  PINS_COUNT
+};
 /*----------------------------------------------------------------------------
  *        Pins
  *----------------------------------------------------------------------------*/
 
+// Not really used
 // Number of pins defined in PinDescription array
-#define PINS_COUNT           (90u)
 #define NUM_DIGITAL_PINS     (23u)
-#define NUM_ANALOG_INPUTS    (10u)
-#define NUM_ANALOG_OUTPUTS   (2u)
+#define NUM_ANALOG_INPUTS    (6u)
+#define NUM_ANALOG_OUTPUTS   (1u)
 #define analogInputToDigitalPin(p)  ((p < 6u) ? (p) + 14u : -1)
 
 #define digitalPinToPort(P)        ( &(PORT->Group[g_APinDescription[P].ulPort]) )
@@ -71,188 +187,93 @@ extern "C"
 #define portModeRegister(port)     ( &(port->DIR.reg) )
 #define digitalPinHasPWM(P)        ( g_APinDescription[P].ulPWMChannel != NOT_ON_PWM || g_APinDescription[P].ulTCChannel != NOT_ON_TIMER )
 
-/*
- * digitalPinToTimer(..) is AVR-specific and is not defined for SAMD
- * architecture. If you need to check if a pin supports PWM you must
- * use digitalPinHasPWM(..).
- *
- * https://github.com/arduino/Arduino/issues/1833
- */
-// #define digitalPinToTimer(P)
+// additional symbol definitions used by arduino core
 
-// Port define : Pin header for shield
-#define PA02                 (50u) //A0
-#define PA03                 (51u) // A5 // AREF
-#define PB04                 (44u) //SWITCH_BUILTIN
-#define PB05                 (45u) //LED_BUILTIN
-#define PB06                 (46u) //A7
-#define PB07                 (52u) //A8
-#define PB08                 (53u) //A2
-#define PB09                 (54u) //A3
-#define PA04                 (55u) //A4
-#define PA05                 (56u) //A1
-#define PA12                 (57u) //SDA1
-#define PA13                 (58u) //SCL1
-#define PB03                 (59u)
-#define PB02                 (60u)
-#define PB01                 (61u) //A9
-#define PB00                 (62u)
-#define PB31                 (63u)
-#define PB30                 (64u)
-#define PA27                 (65u)
-#define PB23                 (66u) //MOSI
-#define PB22                 (67u) //MISO
-#define PA19                 (68u)
-#define PA18                 (69u)
-#define PA17                 (70u) //SCL0 //SCK
-#define PA16                 (71u) //SDA0
-
-#define PB17                 (72u) //LED_RX_CAN0
-#define PB16                 (73u) //LED_TX_CAN0
-#define PB13                 (74u) //LED_RX_CAN1
-#define PB12                 (75u) //LED_TX_CAN1
-
-#define PA14                 (76u) //FAULT_CAN1
-#define PA15                 (77u) //S_CAN1
-#define PA20                 (78u) //FAULT_CAN0
-#define PA21                 (79u) //S_CAN0
-
-#define PA06                 (80u) //SD_CP
-#define PA07                 (81u) //SD_WP
-#define PA08                 (82u) //SD_CMD
-#define PA09                 (83u) //SDDAT0
-#define PA10                 (84u) //SD_DAT1
-#define PA11                 (85u) //SD_DAT2
-#define PB10                 (86u) //SD_DAT3
-#define PB11                 (87u) //SD_CLK
-
-// LED AND SWITCH
-#define LED_BUILTIN          PB05
-#define SWITCH_BUILTIN       PB04
-#define LED_RX_CAN0          PB17
-#define LED_TX_CAN0          PB16
-#define LED_RX_CAN1          PB13
-#define LED_TX_CAN1          PB12
-
-/*
- * Analog pins
- */
- #define PIN_A0               (14ul) // alternative use of PA02
- #define PIN_A1               (PIN_A0 + 1) // alternative use of PA05
- #define PIN_A2               (PIN_A0 + 2) // alternative use of PAB08
- #define PIN_A3               (PIN_A0 + 3) // alternative use of PB09
- #define PIN_A4               (PIN_A0 + 4) // alternative use of PA04
- #define PIN_A5               (PIN_A0 + 5) // alternative use of PA03
- #define PIN_A6               (PIN_A0 + 6) // alternative use of PB00
- #define PIN_A7               (47u) // alternative use of PB06
- #define PIN_A8               (48u) // alternative use of PB07
- #define PIN_A9               (49u) // alternative use of
- static const uint8_t A0  = PIN_A0;
- static const uint8_t A1  = PIN_A1;
- static const uint8_t A2  = PIN_A2;
- static const uint8_t A3  = PIN_A3;
- static const uint8_t A4  = PIN_A4;
- static const uint8_t A5  = PIN_A5;
- static const uint8_t A6  = PIN_A6 ;
- static const uint8_t A7  = PIN_A7;
- static const uint8_t A8  = PIN_A8;
- static const uint8_t A9  = PIN_A9 ;
-
-#define PIN_DAC0             (32u) //DAC0 alternative use of PA02
-#define PIN_DAC1             (33u) //DAC1 alternative use of PA05
-static const uint8_t DAC0 = PIN_DAC0;
-static const uint8_t DAC1 = PIN_DAC1;
+#define PIN_DAC0             DAC0     // PA02
+#define PIN_DAC1             DAC1     // PA05
 
 #define ADC_RESOLUTION		12
+static const uint8_t  A0 = PIN0_ADC0;
 
-// AREF
-#define PIN_ATN              (31ul) //aletrnative use of PA03
-static const uint8_t ATN = PIN_ATN; //PA03
-static const uint8_t AREF = PIN_ATN; // PA03
+// Other pins
+#define PIN_ATN              (AREF)
+static const uint8_t ATN = PIN_ATN;
 
 /*
  * Serial interfaces
  */
 
-// Serial1
-#define PIN_SERIAL1_RX       (0ul)
-#define PIN_SERIAL1_TX       (1ul)
 #define PAD_SERIAL1_RX       (SERCOM_RX_PAD_1)
 #define PAD_SERIAL1_TX       (UART_TX_PAD_0)
 
 /*
  * SPI Interfaces
  */
+
 #define SPI_INTERFACES_COUNT 1
 
-#define PIN_SPI_MISO         (23u)
-#define PIN_SPI_MOSI         (24u)
-#define PIN_SPI_SCK          (25u)
 #define PERIPH_SPI           sercom1
 #define PAD_SPI_TX           SPI_PAD_3_SCK_1
 #define PAD_SPI_RX           SERCOM_RX_PAD_2
 
 static const uint8_t SS	  = 9 ;	// SERCOM1 last PAD is present on d9 but HW SS isn't used. Set here only for reference.
-static const uint8_t MOSI = PIN_SPI_MOSI ;
-static const uint8_t MISO = PIN_SPI_MISO ;
-static const uint8_t SCK  = PIN_SPI_SCK ;
 
 /*
  * Wire Interfaces
  */
-#define WIRE_INTERFACES_COUNT 1
 
-#define PIN_WIRE_SDA         (21u)
-#define PIN_WIRE_SCL         (22u)
-#define PERIPH_WIRE          sercom2
-#define WIRE_IT_HANDLER      SERCOM2_Handler
-#define WIRE_IT_HANDLER_0    SERCOM2_0_Handler
-#define WIRE_IT_HANDLER_1    SERCOM2_1_Handler
-#define WIRE_IT_HANDLER_2    SERCOM2_2_Handler
-#define WIRE_IT_HANDLER_3    SERCOM2_3_Handler
+#define WIRE_INTERFACES_COUNT 2
 
-static const uint8_t SDA1 = PIN_WIRE_SDA; //PA12
-static const uint8_t SCL1 = PIN_WIRE_SCL; //PA13
+#define PIN_WIRE_SDA         (SDA0)
+#define PIN_WIRE_SCL         (SCL0)
+#define PERIPH_WIRE          sercom1
+#define WIRE_IT_HANDLER      SERCOM1_Handler
+#define WIRE_IT_HANDLER_0    SERCOM1_0_Handler
+#define WIRE_IT_HANDLER_1    SERCOM1_1_Handler
+#define WIRE_IT_HANDLER_2    SERCOM1_2_Handler
+#define WIRE_IT_HANDLER_3    SERCOM1_3_Handler
 
-
-// A FINALISER POUR AVOIR 2 I2C
-static const uint8_t SDA0 = PA16; //PA16
-static const uint8_t SCL0 = PA17; //PA17
-
+#define PIN_WIRE1_SDA         (SDA1)
+#define PIN_WIRE1_SCL         (SCL1)
+#define PERIPH_WIRE1          sercom2
+#define WIRE1_IT_HANDLER      SERCOM2_Handler
+#define WIRE1_IT_HANDLER_0    SERCOM2_0_Handler
+#define WIRE1_IT_HANDLER_1    SERCOM2_1_Handler
+#define WIRE1_IT_HANDLER_2    SERCOM2_2_Handler
+#define WIRE1_IT_HANDLER_3    SERCOM2_3_Handler
 
 /*
  * USB
  */
-#define PIN_USB_HOST_ENABLE (28ul)
-#define PIN_USB_DM          (29ul)
-#define PIN_USB_DP          (30ul)
-
+#define PIN_USB_HOST_ENABLE (USB_HOST_ENABLE)
+#define PIN_USB_DM          (USB_DM)
+#define PIN_USB_DP          (USB_DP)
 
 /*
  * I2S Interfaces
- */
-//#define I2S_INTERFACES_COUNT 1
+ 
+#define I2S_INTERFACES_COUNT 1
 
-//#define I2S_DEVICE          0
-//#define I2S_CLOCK_GENERATOR 3
+#define I2S_DEVICE 0
+#define I2S_CLOCK_GENERATOR 3
 
-//#define PIN_I2S_SDO          (11u)
-//#define PIN_I2S_SDI          (12u)
-//#define PIN_I2S_SCK          PIN_SERIAL1_TX
-//#define PIN_I2S_FS           (10u)
-//#define PIN_I2S_MCK          PIN_SERIAL1_RX
-
-// On-board QSPI Flash -> à modifier pour que ça marche avec une carte SD !
+#define PIN_I2S_SDO (11u)
+#define PIN_I2S_SDI (12u)
+#define PIN_I2S_SCK PIN_SERIAL1_TX
+#define PIN_I2S_FS (10u)
+#define PIN_I2S_MCK PIN_SERIAL1_RX
+*/
+// On-board QSPI Flash
 #define EXTERNAL_FLASH_DEVICES   GD25Q16C
 #define EXTERNAL_FLASH_USE_QSPI
 
-//QSPI Pins -> déjà été modifié dans .cpp
-#define PIN_QSPI_SCK    (34u)
-#define PIN_QSPI_CS     (35u)
-#define PIN_QSPI_IO0    (36u)
-#define PIN_QSPI_IO1    (37u)
-#define PIN_QSPI_IO2    (38u)
-#define PIN_QSPI_IO3    (39u)
+//QSPI Pins
+#define PIN_QSPI_SCK    (ENUM_QSPI_SCK)
+#define PIN_QSPI_CS     (ENUM_CS)
+#define PIN_QSPI_IO0    (IO0)
+#define PIN_QSPI_IO1    (IO1)
+#define PIN_QSPI_IO2    (IO2)
+#define PIN_QSPI_IO3    (IO3)
 
 #if !defined(VARIANT_QSPI_BAUD_DEFAULT)
   // TODO: meaningful value for this
@@ -262,15 +283,10 @@ static const uint8_t SCL0 = PA17; //PA17
 /*
  * CAN
  */
- //CAN1
-#define PIN_CAN_STANDBY (40) // voir si le code fonctionne sans
-#define PIN_CAN_BOOSTEN (41) // voir si le code fonctionne sans
-#define PIN_CAN_TX      (42)
-#define PIN_CAN_RX      (43)
-
-//CAN0
-#define PIN_CAN1_TX     (88) //(12)
-#define PIN_CAN1_RX     (89) //(13)
+//#define PIN_CAN_STANDBY (40)
+//#define PIN_CAN_BOOSTEN (41)
+#define PIN_CAN_TX      (PIN_CAN0_RX)
+#define PIN_CAN_RX      (PIN_CAN0_RX)
 
 
 #ifdef __cplusplus
@@ -320,3 +336,4 @@ extern Uart Serial1;
 #define SERIAL_PORT_HARDWARE_OPEN   Serial1
 
 #endif /* _VARIANT_FEATHER_M4_ */
+
