549b4200fff39b1312a356f2e61ca5ccb3aa099a
John Taylor <john.taylor@epfl.ch>
2023-01-31 20:44:53 +0100

    fixed can library linking error

--- customboard/libraries/arduino-CAN-master/src/CANSAME5x.cpp
+++ customboard/libraries/arduino-CAN-master/src/CANSAME5x.cpp
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full
 // license information.
 
-#if defined(ADAFRUIT_FEATHER_M4_CAN)
 #include <stdint.h>
 #include <stdlib.h>
 #include "CANSAME5x.h"
@@ -603,5 +602,3 @@ CANSAME5x *CANSAME5x::instances[2];
 
 CANSAME5x can0;
 CANSAME5x can1(PIN_CAN1_TX, PIN_CAN1_RX);
-
-#endif
